a8458be92539b3b16a8898b58b96ec0f3ad79d33
grillazz <the@grillazz.com>
2021-11-27 19:13:06 +0100

    enable anyio for unit tests

--- tests/api/test_stuff.py
+++ tests/api/test_stuff.py
@@ -4,9 +4,7 @@ import pytest
 from fastapi import status
 from httpx import AsyncClient
 
-# decorate all tests with @pytest.mark.asyncio
-pytestmark = pytest.mark.asyncio
-
+pytestmark = pytest.mark.anyio
 
 @pytest.mark.parametrize(
     "payload, status_code",
