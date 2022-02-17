327792954b86c1260f2c0d83778fde0aaaf8703d
grillazz <the@grillazz.com>
2022-02-17 19:56:10 +0100

    wrap db session with context manager

--- the_app/database.py
+++ the_app/database.py
@@ -1,5 +1,6 @@
 from typing import AsyncGenerator
 
+from fastapi import HTTPException
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
@@ -22,13 +23,15 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 
 # Dependency
 async def get_db() -> AsyncGenerator:
-    session = async_session()
-    try:
-        yield session
-        await session.commit()
-    except SQLAlchemyError as ex:
-        await session.rollback()
-        raise ex
-    finally:
-        await session.close()
-
+    async with async_session() as session:
+        try:
+            yield session
+            await session.commit()
+        except SQLAlchemyError as sql_ex:
+            await session.rollback()
+            raise sql_ex
+        except HTTPException as http_ex:
+            session.rollback()
+            raise http_ex
+        finally:
+            await session.close()
