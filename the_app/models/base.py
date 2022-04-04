5ee9005ebb749e3f513f515d4774a1f76fa47c15
grillazz <the@grillazz.com>
2022-04-04 20:22:21 +0200

    add read only declarative base for shakespeare models

--- the_app/models/base.py
+++ the_app/models/base.py
@@ -7,6 +7,17 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
 @as_declarative()
+class BaseReadOnly:
+    id: Any
+    __name__: str
+    # Generate __tablename__ automatically
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+
+@as_declarative()
 class Base:
     id: Any
     __name__: str
