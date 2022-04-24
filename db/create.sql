cbdae4da0608defd4641f1d6261b8c3a93840930
Jakub Miazek <the@grillazz.com>
2022-04-24 15:27:21 +0200

    add schema create to sql script

--- db/create.sql
+++ db/create.sql
@@ -1,4 +1,9 @@
 DROP DATABASE IF EXISTS devdb;
 CREATE DATABASE devdb;
+\connect devdb;
+CREATE SCHEMA shakespeare;
+
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+\connect testdb;
+CREATE SCHEMA shakespeare;
