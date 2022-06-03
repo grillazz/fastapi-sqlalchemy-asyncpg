32c5654dd5b318e80bafb260a73899a375a51925
Jakub Miazek <the@grillazz.com>
2022-06-03 19:22:50 +0200

    add db schemas to init sql

--- db/create.sql
+++ db/create.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS devdb;
 CREATE DATABASE devdb;
 \connect devdb;
-CREATE SCHEMA shakespeare;
-CREATE SCHEMA the_others;
+CREATE SCHEMA shakespeare_i_like;
+CREATE SCHEMA happy_hog;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 \connect testdb;
-CREATE SCHEMA shakespeare;
-CREATE SCHEMA the_others;
+CREATE SCHEMA shakespeare_i_like;
+CREATE SCHEMA happy_hog;
