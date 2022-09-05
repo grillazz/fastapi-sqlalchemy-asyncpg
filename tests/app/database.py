b736de67100a856bae163e2f6e26eca99d771a78
Jakub Miazek <the@grillazz.com>
2022-09-05 14:58:45 +0200

    refactor

---
+++ tests/app/database.py
@@ -0,0 +1,37 @@
+from typing import AsyncGenerator
+
+from fastapi import HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from the_app import config
+
+global_settings = config.get_settings()
+url = global_settings.asyncpg_url
+
+engine = create_async_engine(
+    url,
+    future=True,
+    echo=True,
+)
+
+# expire_on_commit=False will prevent attributes from being expired
+# after commit.
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+# Dependency
+async def get_db() -> AsyncGenerator:
+    async with async_session() as session:
+        try:
+            yield session
+            await session.commit()
+        except SQLAlchemyError as sql_ex:
+            await session.rollback()
+            raise sql_ex
+        except HTTPException as http_ex:
+            await session.rollback()
+            raise http_ex
+        finally:
+            await session.close()
---
+++ tests/app/models/nonsense.py
@@ -0,0 +1,42 @@
+import uuid
+
+from fastapi import HTTPException, status
+from sqlalchemy import Column, String, select
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from the_app.models.base import Base
+
+
+class Nonsense(Base):
+    __tablename__ = "nonsense"
+    __table_args__ = (
+        {"schema": "happy_hog"},
+    )
+    id = Column(UUID(as_uuid=True), unique=True, default=uuid.uuid4, autoincrement=True)
+    name = Column(String, nullable=False, primary_key=True, unique=True)
+    description = Column(String, nullable=False)
+
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+
+
+    @classmethod
+    async def find(cls, db_session: AsyncSession, name: str):
+        """
+
+        :param db_session:
+        :param name:
+        :return:
+        """
+        stmt = select(cls).where(cls.name == name)
+        result = await db_session.execute(stmt)
+        instance = result.scalars().first()
+        if instance is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail={"Record not found": f"There is no record for requested name value : {name}"},
+            )
+        else:
+            return instance
