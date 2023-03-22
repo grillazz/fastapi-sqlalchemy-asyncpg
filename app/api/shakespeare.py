fbf2b339ade55b23dc24feaeaf8b562675e28c57
Jakub Miazek <the@grillazz.com>
2023-03-22 21:20:02 +0100

    try query param annotations

--- app/api/shakespeare.py
+++ app/api/shakespeare.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+from pydantic import Required
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_db
@@ -11,7 +14,7 @@ router = APIRouter(prefix="/v1/shakespeare")
     "/",
 )
 async def find_paragraph(
-    character: str,
+    character: Annotated[str, Query(description="Character name")] = Required,
     db_session: AsyncSession = Depends(get_db),
 ):
     return await Paragraph.find(db_session=db_session, character=character)
