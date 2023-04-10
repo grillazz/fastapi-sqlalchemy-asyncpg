429909344d58bb856fd949bab9d92384e978dc25
Jakub Miazek <the@grillazz.com>
2023-04-10 21:42:00 +0200

    unify logging with rich

---
+++ app/logging.py
@@ -0,0 +1,22 @@
+import logging
+
+from rich.console import Console
+from rich.logging import RichHandler
+
+
+from app.utils import SingletonMeta
+
+
+class AppLogger(metaclass=SingletonMeta):
+    _logger = None
+
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+
+    def get_logger(self):
+        return self._logger
+
+
+class RichConsoleHandler(RichHandler):
+    def __init__(self, width=200, style=None, **kwargs):
+        super().__init__(console=Console(color_system="256", width=width, style=style), **kwargs)
