a1fbb86babe73469ab4c6158258bea098e154768
Jakub Miazek <jakub.miazek@buildgrassroots.com>
2023-07-24 12:49:07 +0200

    update project setting with new env vars

--- app/config.py
+++ app/config.py
@@ -1,12 +1,16 @@
 import os
 from functools import lru_cache
 
-from pydantic import PostgresDsn
+from pydantic import PostgresDsn, RedisDsn
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     asyncpg_url: PostgresDsn = os.getenv("SQL_URL")
+    secret_key: str = os.getenv("FERNET_KEY")
+    redis_url: RedisDsn = os.getenv("REDIS_URL")
+    jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
+    jwt_expire: int = os.getenv("JWT_EXPIRE")
 
 
 @lru_cache
