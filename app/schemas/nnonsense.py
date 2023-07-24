0b2d2e79338bb564bbb5ecbe2102ab7443573ecf
Jakub Miazek <jakub.miazek@buildgrassroots.com>
2023-07-24 12:53:57 +0200

    introduce pydantic config dict

--- app/schemas/nnonsense.py
+++ app/schemas/nnonsense.py
@@ -1,9 +1,12 @@
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
+
+config = ConfigDict(from_attributes=True)
 
 
 class NonsenseSchema(BaseModel):
+    model_config = config
     name: str = Field(
         title="",
         description="",
@@ -13,17 +16,18 @@ class NonsenseSchema(BaseModel):
         description="",
     )
 
-    class Config:
-        from_attributes = True
-        json_schema_extra = {
-            "example": {
-                "name": "Name for Some Nonsense",
-                "description": "Some Nonsense Description",
-            }
-        }
+    # class Config:
+    #     from_attributes = True
+    #     json_schema_extra = {
+    #         "example": {
+    #             "name": "Name for Some Nonsense",
+    #             "description": "Some Nonsense Description",
+    #         }
+    #     }
 
 
 class NonsenseResponse(BaseModel):
+    model_config = config
     id: UUID = Field(
         title="Id",
         description="",
@@ -37,12 +41,12 @@ class NonsenseResponse(BaseModel):
         description="",
     )
 
-    class Config:
-        from_attributes = True
-        json_schema_extra = {
-            "example": {
-                "config_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                "name": "Name for Some Nonsense",
-                "description": "Some Nonsense Description",
-            }
-        }
+    # class Config:
+    #     from_attributes = True
+    #     json_schema_extra = {
+    #         "example": {
+    #             "config_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    #             "name": "Name for Some Nonsense",
+    #             "description": "Some Nonsense Description",
+    #         }
+    #     }
