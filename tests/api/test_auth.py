c4cee5c5cfa30a6ec61e232df356ce4e0b864f9f
Jakub Miazek <the@grillazz.com>
2023-09-02 20:01:21 +0200

    typo :)

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -8,7 +8,7 @@ pytestmark = pytest.mark.anyio
 
 # TODO: parametrize test with diff urls
 async def test_add_user(client: AsyncClient):
-    payload = {"email": "rancher@grassroots.com", "first_name": "Joe", "last_name": "Garcia", "password": "s1lly"}
+    payload = {"email": "joe@grillazz.com", "first_name": "Joe", "last_name": "Garcia", "password": "s1lly"}
     response = await client.post("/user/", json=payload)
     assert response.status_code == status.HTTP_201_CREATED
     claimset = jwt.decode(response.json()["access_token"], options={"verify_signature": False})
@@ -19,7 +19,7 @@ async def test_add_user(client: AsyncClient):
 
 # TODO: parametrize test with diff urls including 404 and 401
 async def test_get_token(client: AsyncClient):
-    payload = {"email": "rancher@grassroots.com", "password": "s1lly"}
+    payload = {"email": "joe@grillazz.com", "password": "s1lly"}
     response = await client.post("/user/token", json=payload)
     assert response.status_code == status.HTTP_201_CREATED
     claimset = jwt.decode(response.json()["access_token"], options={"verify_signature": False})
