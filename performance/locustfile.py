a6cceeef73d904a0469a8b2ab13465d97c452503
Jakub Miazek <the@grillazz.com>
2024-05-12 19:52:51 +0200

    fix unit tests

--- performance/locustfile.py
+++ performance/locustfile.py
@@ -5,9 +5,9 @@ class Stuff(HttpUser):
     wait_time = between(1, 3)
 
     @task
-    def test_find(self):
+    def find_stuff(self):
         self.client.get("/v1/stuff/string")
 
     @task
-    def test_find_pool(self):
+    def find_stuff_with_pool(self):
         self.client.get("/v1/stuff/pool/string")
