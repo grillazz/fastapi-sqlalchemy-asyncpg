0b301b798e8f1c5dab836f133ff7a1ba6e6534eb
Jakub Miazek <jakub.miazek@buildgrassroots.com>
2024-10-17 16:42:09 +0200

    bump project deps

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ async def lifespan(_app: FastAPI):
         await _app.postgres_pool.close()
 
 
-app = FastAPI(title="Stuff And Nonsense API", version="0.15", lifespan=lifespan)
+app = FastAPI(title="Stuff And Nonsense API", version="0.16", lifespan=lifespan)
 
 app.include_router(stuff_router)
 app.include_router(nonsense_router)
