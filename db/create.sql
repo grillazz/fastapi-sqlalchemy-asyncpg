9147f086a40bcb2beff367cb2e6bb0b99d1246f7
Jakub Miazek <the@grillazz.com>
2024-10-20 09:23:48 +0200

    add scheduler db schema

--- db/create.sql
+++ db/create.sql
@@ -1,11 +1,11 @@
--- DROP DATABASE IF EXISTS devdb;
--- CREATE DATABASE devdb;
 \connect devdb;
 CREATE SCHEMA shakespeare;
 CREATE SCHEMA happy_hog;
+CREATE SCHEMA scheduler;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 \connect testdb;
 CREATE SCHEMA shakespeare;
 CREATE SCHEMA happy_hog;
+CREATE SCHEMA scheduler;
