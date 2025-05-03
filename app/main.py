1527b76b0311720fcf5abd13594f2153263dcfd1
grillazz <the@grillazz.com>
2025-05-03 08:21:42 +0200

    bump project deps

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ async def lifespan(_app: FastAPI):
         await _app.postgres_pool.close()
 
 
-app = FastAPI(title="Stuff And Nonsense API", version="0.17", lifespan=lifespan)
+app = FastAPI(title="Stuff And Nonsense API", version="0.18.0", lifespan=lifespan)
 
 app.include_router(stuff_router)
 app.include_router(nonsense_router)
